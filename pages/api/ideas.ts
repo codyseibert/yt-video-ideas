bbc4008b136432d29ddd00e6558055cd57200642
boybenson <ybenson96@gmail.com>
2022-07-18 15:30:58 +0000

    fix: added env to gitignore

---
+++ pages/api/ideas.ts
@@ -0,0 +1,9 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from "axios";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  console.log("here");
+};
+
+export default handler;
